7cd77cacbf204307ac5b7b242b46bdac6037fc8f
aeon077 <carolyn.schiltz@gmail.com>
2020-10-16 20:05:33 -0400

    change to homepage links

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,6 @@ const IndexPage = () => {
       <h1>Hello, World</h1>
       <h2>I'm Carolyn, a FullStack Developer, living in beautiful Ponte Vedra, Florida.</h2>
       <p><Link to="/about">About me</Link></p>
-      <p>Need a developer? <Link to="/contact">Contact Me!</Link></p>
     </Layout>
   )
 }
